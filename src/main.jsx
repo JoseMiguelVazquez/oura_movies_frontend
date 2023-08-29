80e5883589834b618326ece1d3e755be6efe4f76
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-08-28 20:06:38 -0600

    bootstrap added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+// Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+// Bootstrap Bundle JS
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import './index.css'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
