b2e3d22ad30a9e66ae7f15fbf616d209314a4efe
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-08-28 21:50:45 -0600

    showing movies in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,55 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
+import { getMovies, reset } from "../features/movies/movieSlice"
+import Spinner from '../components/Spinner'
+
 const Home = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { user } = useSelector((state) => state.auth)
+  const { movies, isLoading, isError, message } = useSelector((state) => state.movie)
+
+  useEffect(() => {
+    if(isError) {
+      console.log(message)
+    }
+
+    if(!user) {
+      navigate('/login')
+    } else {
+      dispatch(getMovies())
+    }
+
+    return ()=> {
+      dispatch(reset())
+    }
+
+  }, [user, navigate, isError, message, dispatch])
+
+  if(isLoading) {
+    return <Spinner/>
+  }
+
+
   return (
-    <div>
-      Home
+    <div className="container">
+      <h1>Oura Movies</h1>
+      <section>
+        {movies.length > 0 ? (
+          <div>
+            {movies.map((movie)=>(
+              //comienza el componente
+              <div>
+                <h2>{movie.title}</h2>
+                <img src={movie.backdrop_url} alt="backdrop" />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <h3>There are not tasks for this user</h3>
+        )}
+      </section>
     </div>
   )
 }
